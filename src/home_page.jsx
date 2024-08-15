35672c798964883a596364d5d33ab2691aee4bcb
csant09 <shishantstha@gmail.com>
2024-08-15 08:03:49 +0545

    commit

---
+++ src/home_page.jsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+    var text = "Taylor Swift";
+    return(
+        <>
+            <p><b>{text}</b></p>
+            <img src="https://i1.sndcdn.com/artworks-godyHoBYCezrTtt7-T5vszQ-t500x500.jpg" height={200} width={200}/>
+        </>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App, { Title } from './App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './home_page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HomePage/>
   </React.StrictMode>
 );
 
