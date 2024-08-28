d5575808f8411838d49a1959d0977269e52d88d3
csant26 <shishantstha@gmail.com>
2024-08-28 07:22:23 +0545

    commit

--- src/pages/downloads.jsx
+++ src/pages/downloads.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/navbar"
 const Downloads = () => {
+    
     return(
         <>
         <Navbar title={"Downloads"}/>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import Navbar from "../components/navbar";
+import "../styles/home.css";
 
 export default function Home(){
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () =>{
+        setCount(prevCount => prevCount + 1);
+    }
+    const handleDecrement = () =>{
+        setCount(prevCount => prevCount - 1);
+    }
     return(
         <>
         <Navbar title={"Home Page"}/>
+        <p id="center-text">{count}</p>
+        <div id="button" onClick={handleIncrement}>+</div>
         </>
     );
 }
