3b91a3277a637ac7ddaf2da373941caf06b06193
csant26 <shishantstha@gmail.com>
2024-09-11 09:13:52 +0545

    commit

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from 'react';
 import '../styles/user.css'
+import UserCard from '../components/user_card';
 
 const Users = ()=>{
+
+    const [allUsers,setAllUsers] = useState([]);
+
+    //if set to empty array, called once in each render
+    useEffect(
+        ()=>{
+            //fetch vaneko promise type return garchha i.e async/await tesaile then.
+            fetch("https://jsonplaceholder.typicode.com/users").then(
+                async (response)=>{
+                    const users = await response.json()
+                    console.log(users);
+                    setAllUsers(users)
+                }
+            )
+        },
+        []
+    )
     return(
         <div id='users'>
             <h1 id='title'>All Users</h1>
-            <div id='user-grid'></div>
+            <div id='user-grid'>
+                {
+                    allUsers.map(
+                        (user)=>{
+                            return <UserCard/>
+                        }
+                    )
+                }
+            </div>
         </div>
     );
 }
