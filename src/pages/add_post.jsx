951254c078b5261e02c7c7075cd6f20062ba9984
csant26 <shishantstha@gmail.com>
2024-09-25 09:28:56 +0545

    commit

--- src/pages/add_post.jsx
+++ src/pages/add_post.jsx
@@ -5,15 +5,60 @@ const AddPost=()=>{
     const [title,setTitle]=useState("");
     const [description,setDescription]=useState("");
     const [userId, setUserId]=useState("1");
-    const handleSubmit=(event)=>{
+    const [isloading,setIsLoading] = useState(false)
+    const handleSubmit= async (event)=>{
        event.preventDefault();
-       console.log("Title",title);
-       console.log("Description",description);
-       console.log("User Id",userId);
+       if(title==""){
+        alert("Please enter title.");
+        return
+       }
+       if(title.length<=2){
+        alert("Title too short.");
+        return
+       }
+       if(description==""){
+        alert("Please enter description.")
+        return
+       }
+       if(userId==""){
+        alert("Please enter user id")
+        return
+       }
+       setIsLoading(true);
+       try{
+        const response= await fetch(
+            "https://jsonplaceholder.typicode.com/posts",{
+               method:"POST",
+               body:JSON.stringify(
+                {
+                    title: title,
+                    description: description,
+                    userId: userId
+                }
+               ),
+               headers:{
+                "Content-Type":"application/json",
+               },
+            }
+           );
+           const data = await response.json();
+           console.log("Title",title);
+           console.log("Description",description);
+           console.log("User Id",userId);
+           console.log(data);
+           alert("Post created successfully.");
+       }
+       catch(exception){
+        alert("Something went wrongg")
+       }
+       setIsLoading(false);
     }
     return(
         <form onSubmit={handleSubmit}>
             <h1>Add Post</h1>
+            {
+                isloading && <p>Loading.......</p>
+            }
             <PostForm 
               onChangeTitle={
                 (e)=>setTitle(e.target.value)}
@@ -24,7 +69,7 @@ const AddPost=()=>{
                 (e)=> setUserId(e.target.value)
               }
             />
-        </form>
+        </form> 
     )
 }
 export default AddPost;
